8adae844d2123977f7239bc92b665a3b44f73a75
mim <mim3dot@gmail.com>
2021-09-22 22:25:46 -0500

    Added the initial place-holder landing page for the project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import logo from './fg_01.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-conitainer">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='middle-text'>Future's coming.</div>
+        <div className="colorful-backgrounds bottom-section"></div>
       </header>
     </div>
   );
